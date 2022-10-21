a0f6bfcb27d9c6d98d6d2070b0a3edac01ab2530
NidhalSaidane <nidhalsaidane94@gmail.com>
2022-10-21 11:11:06 +0100

    jsx

---
+++ src/component/Profile/ProfilePhoto.js
@@ -0,0 +1,14 @@
+import React from "react";
+import nidhal from "./nidhal.png"
+
+function ProfilePhoto() {
+    return(
+        <div>
+      <img src={nidhal} className="App-logo" alt="profile logo" /> 
+            
+        </div>
+        
+    )
+    
+}
+export default ProfilePhoto
